1ba896250815fb65ea4c0b25b5b650015eabbae5
Ariel Sanchez <arii.sanchez123@gmail.com>
2020-08-08 17:03:40 -0300

    Cambios minimos

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-class Vehiculo {
+const datos = [
+  {
+    marca: "Peugeot",
+    modelo: "206",
+    puertas: 4,
+    precio: 200000.00
+  },
+  {
+    marca: "Honda",
+    modelo: "Titan",
+    cilindrada: "125c",
+    precio: 60000.00
+  },
+  {
+    marca: "Peugeot",
+    modelo: "208",
+    puertas: 5,
+    precio: 250000.00
+  },
+  {
+    marca: "Yamaha",
+    modelo: "YBR",
+    cilindrada: "160c",
+    precio: 80500.50
+  }
+]
+
+class Vehiculos {
   todos = []
 
   constructor(array) {
@@ -22,9 +49,9 @@ class Vehiculo {
   listarTodos() {
     this.todos.forEach(element => {
       if (element instanceof Auto) {
-        console.log(`Marca: ${element.marca} // Modelo: ${element.modelo} // Puertas: ${element.puertas}  // Precio: ${this.priceToString(element.precio)}`);
+        console.log(`Marca: ${element.marca} // Modelo: ${element.modelo} // Puertas: ${element.puertas}  // Precio: ${this.precioATexto(element.precio)}`);
       } else if (element instanceof Moto) {
-        console.log(`Marca: ${element.marca} // Modelo: ${element.modelo} // Cilindrada: ${element.cilindrada}  // Precio: ${this.priceToString(element.precio)}`);
+        console.log(`Marca: ${element.marca} // Modelo: ${element.modelo} // Cilindrada: ${element.cilindrada}  // Precio: ${this.precioATexto(element.precio)}`);
       } else {
         console.log("Error!");
       }
@@ -48,7 +75,7 @@ class Vehiculo {
   contieneLetra(letra) {
     const vehiculo = this.todos.find(element => element.modelo.includes(letra));
     if (vehiculo) {
-      console.log(`Vehículo que contiene en el modelo la letra '${letra}': ${vehiculo.marca} ${vehiculo.modelo} ${this.priceToString(vehiculo.precio)}`);
+      console.log(`Vehículo que contiene en el modelo la letra '${letra}': ${vehiculo.marca} ${vehiculo.modelo} ${this.precioATexto(vehiculo.precio)}`);
     } else {
       console.log('Error!');
     }
@@ -75,8 +102,8 @@ class Vehiculo {
     console.log('=============================');
   }
 
-  priceToString(priceNumber) {
-    let price = new Intl.NumberFormat("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(priceNumber);
+  precioATexto(precio) {
+    let price = new Intl.NumberFormat("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(precio);
     price = price.replace(new RegExp('[,]', 'g'), '#');
     price = price.replace('.', ',');
     price = price.replace(new RegExp('[#]', 'g'), '.');
@@ -115,35 +142,14 @@ class Auto {
   }
 }
 
-const array = [
-  {
-    marca: "Peugeot",
-    modelo: "206",
-    puertas: 4,
-    precio: 200000.00
-  },
-  {
-    marca: "Honda",
-    modelo: "Titan",
-    cilindrada: "125c",
-    precio: 60000.00
-  },
-  {
-    marca: "Peugeot",
-    modelo: "208",
-    puertas: 5,
-    precio: 250000.00
-  },
-  {
-    marca: "Yamaha",
-    modelo: "YBR",
-    cilindrada: "160c",
-    precio: 80500.50
-  }
-]
-
-const vehiculos = new Vehiculo(array);
-
-
+const vehiculos = new Vehiculos(datos);
 vehiculos.printAllData()
-vehiculos.listarTodos()
+
+/*
+### NOTA
+
+La funcion Intl.NumberFormat no funciona correctamente
+ejecutando directamente con Node (debido a su instalacion por defecto), pero si desde la consola de un navegador.
+Tuve que forzar el formato del precio en 'en-US' y posteriormente
+reemplazar los simbolos convenientes para la correcta visualizacion. (funcion precioATexto)
+*/
